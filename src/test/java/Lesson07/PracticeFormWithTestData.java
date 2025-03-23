c9868e9d3fe3faa3e2509d591db4ea9878f83fbc
Ilya Khabarov <khabarov.ilya@gmail.com>
2025-03-23 22:25:20 +0300

    added TestData

---
+++ src/test/java/Lesson07/PracticeFormWithTestData.java
@@ -0,0 +1,68 @@
+package Lesson07;
+
+import Lesson07.pages.PracticeFormPage;
+import org.junit.jupiter.api.Test;
+
+import static Lesson07.TestData.*;
+
+public class PracticeFormWithTestData extends TestBase {
+
+    PracticeFormPage registrationPage = new PracticeFormPage();
+
+    @Test
+    void successRegistrationTest() {
+        registrationPage.openPage()
+                .removeBanner()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(userEmail)
+                .setGender(gender)
+                .setUserNumber(userNumber)
+                .setDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)
+                .setSubjects(subject)
+                .setHobbies(hobbies)
+                .setPicture(uploadImage)
+                .setAddress(currentAddress)
+                .setStateAndCity(state, city)
+                .submitForm();
+
+        registrationPage.checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", userEmail)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", userNumber)
+                .checkResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                .checkResult("Subjects", subject)
+                .checkResult("Hobbies", hobbies)
+                .checkResult("Picture", "qa_guru_logo.png")
+                .checkResult("Address", currentAddress)
+                .checkResult("State and City", state + " " + city);
+    }
+
+    @Test
+    void minimalInputDataTest() {
+        registrationPage.openPage()
+                .removeBanner()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setGender(gender)
+                .setUserNumber(userNumber)
+                .submitForm();
+
+        registrationPage.checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", userNumber);
+    }
+
+    @Test
+    void negativeRegistrationTest() {
+        registrationPage.openPage()
+                .removeBanner()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setGender(gender)
+                .setUserNumber("12345")
+                .submitForm();
+
+        registrationPage.checkInvalidInput();
+    }
+}
---
+++ src/test/java/Lesson07/TestData.java
@@ -0,0 +1,18 @@
+package Lesson07;
+
+public class TestData {
+    public static String firstName = "Ilya",
+            lastName = "Khabarov",
+            userEmail = "test@test.com",
+            gender = "Male",
+            userNumber = "1234567890",
+            dayOfBirth = "06",
+            monthOfBirth = "May",
+            yearOfBirth = "1994",
+            subject = "Computer Science",
+            hobbies = "Sports",
+            uploadImage = "images/qa_guru_logo.png",
+            currentAddress = "Moscow, Russia",
+            state = "Rajasthan",
+            city = "Jaipur";
+}
