24db81d8792a16a9709a7f102f5c18de5074bdb7
Ilya Khabarov <khabarov.ilya@gmail.com>
2025-03-25 22:52:13 +0300

    make some changes

--- src/test/java/lesson07/TestData.java
+++ src/test/java/lesson07/TestData.java
@@ -8,21 +8,21 @@ public class TestData {
 
     private final Faker faker = new Faker(new Locale("en-GB"));
 
-    public String firstName = faker.name().firstName();
-    public String lastName = faker.name().lastName();
-    public String userEmail = faker.internet().emailAddress();
-    public String gender = faker.options().option("Male", "Female", "Other");
-    public String userNumber = faker.phoneNumber().subscriberNumber(10);
-    public String dayOfBirth = String.format("%02d", faker.number().numberBetween(1, 28));
-    public String monthOfBirth = faker.options().option("January", "February", "March", "April", "May", "June", "July",
-            "August", "September", "October", "November", "December");
-    public String yearOfBirth = String.valueOf(faker.number().numberBetween(1990, 2023));
-    public String subject = faker.options().option("Social Studies", "Arts", "Chemistry");
-    public String hobbies = faker.options().option("Sports", "Reading", "Music");
-    public String uploadImage = faker.options().option("images/qa_guru_logo.png");
-    public String currentAddress = faker.address().streetAddress();
-    public String state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
-    public String city = randomCity(state);
+    public String firstName = faker.name().firstName(),
+     lastName = faker.name().lastName(),
+     userEmail = faker.internet().emailAddress(),
+     gender = faker.options().option("Male", "Female", "Other"),
+     userNumber = faker.phoneNumber().subscriberNumber(10),
+     dayOfBirth = String.format("%02d", faker.number().numberBetween(1, 28)),
+     monthOfBirth = faker.options().option("January", "February", "March", "April", "May", "June", "July",
+     "August", "September", "October", "November", "December"),
+     yearOfBirth = String.valueOf(faker.number().numberBetween(1990, 2023)),
+     subject = faker.options().option("Social Studies", "Arts", "Chemistry"),
+     hobbies = faker.options().option("Sports", "Reading", "Music"),
+     uploadImage = faker.options().option("images/qa_guru_logo.png"),
+     currentAddress = faker.address().streetAddress(),
+     state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan"),
+     city = randomCity(state);
 
 
     public String randomCity(String state) {
