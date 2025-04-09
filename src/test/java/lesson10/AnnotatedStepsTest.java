c332d1da41026694eca507e948bcdf87473c1ea9
Ilya Khabarov <khabarov.ilya@gmail.com>
2025-04-10 02:12:37 +0300

    change build.gradle

--- src/test/java/lesson10/AnnotatedStepsTest.java
+++ src/test/java/lesson10/AnnotatedStepsTest.java
@@ -2,6 +2,7 @@ package lesson10;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 
@@ -9,6 +10,7 @@ public class AnnotatedStepsTest {
     private static final String REPO = "selenide/selenide";
 
     @Test
+    @Disabled
     public void annotatedStepsIssueSearchTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         WebSteps steps = new WebSteps();
