81760457aa60b868b3754a038f2241b4c151ba8b
Ilya Khabarov <khabarov.ilya@gmail.com>
2025-04-10 00:56:19 +0300

    add homework for lesson11

---
+++ src/test/java/lesson11/TestBase.java
@@ -0,0 +1,39 @@
+package lesson11;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import lesson11.helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1280x720";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+//        Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
---
+++ src/test/java/lesson11/pages/PracticeFormPage.java
@@ -0,0 +1,113 @@
+package lesson11.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import lesson11.pages.components.CalendarComponent;
+import lesson11.pages.components.ResultTableComponent;
+
+import static com.codeborne.selenide.Condition.cssValue;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormPage {
+
+    private final SelenideElement firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genderSelect = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            calendarInput = $("#dateOfBirthInput"),
+            subjectsInput = $("#subjectsInput"),
+            hobbiesSelect = $("#hobbiesWrapper"),
+            pictureSelect = $("#uploadPicture"),
+            userAddressInput = $("#currentAddress"),
+            stateAndCitySelect = $("#stateCity-wrapper"),
+            submitButton = $("#submit");
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+    ResultTableComponent resultTableComponent = new ResultTableComponent();
+
+    public PracticeFormPage openPage() {
+        open("/automation-practice-form");
+        $(".text-center").shouldHave(text("Practice Form"));
+        return this;
+    }
+
+    public PracticeFormPage removeBanner() {
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        return this;
+    }
+
+    public PracticeFormPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+        return this;
+    }
+
+    public PracticeFormPage setLastName(String value) {
+        lastNameInput.setValue(value);
+        return this;
+    }
+
+    public PracticeFormPage setUserEmail(String value) {
+        userEmailInput.setValue(value);
+        return this;
+    }
+
+    public PracticeFormPage setGender(String value) {
+        genderSelect.$(byText(value)).click();
+        return this;
+    }
+
+    public PracticeFormPage setUserNumber(String value) {
+        userNumberInput.setValue(value);
+        return this;
+    }
+
+    public PracticeFormPage setDateOfBirth(String day, String month, String year) {
+        calendarInput.click();
+        calendarComponent.setDate(day, month, year);
+        return this;
+    }
+
+    public PracticeFormPage setSubjects(String value) {
+        subjectsInput.setValue(value).pressEnter();
+        return this;
+    }
+
+    public PracticeFormPage setHobbies(String value) {
+        hobbiesSelect.$(byText(value)).scrollTo().click();
+        return this;
+    }
+
+    public PracticeFormPage setPicture(String value) {
+        pictureSelect.uploadFromClasspath(value);
+        return this;
+    }
+
+    public PracticeFormPage setAddress(String value) {
+        userAddressInput.setValue(value);
+        return this;
+    }
+
+    public PracticeFormPage setStateAndCity(String state, String city) {
+        $("#state").click();
+        stateAndCitySelect.$(byText(state)).click();
+        $("#city").click();
+        stateAndCitySelect.$(byText(city)).click();
+        return this;
+    }
+
+    public void submitForm() {
+        submitButton.click();
+    }
+
+    public void checkInvalidInput() {
+        userNumberInput.shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+    }
+
+    public PracticeFormPage checkResult(String key, String value){
+        resultTableComponent.checkResult(key, value);
+        return this;
+    }
+}
